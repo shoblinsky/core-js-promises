3042aaaa89c36c14d9e17665fa1386586156f9e8
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-01-16 19:54:57 +0500

    feat: 1 test is passing

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,12 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (Math.abs(number) === number) {
+      resolve(number);
+    } else reject(number);
+  });
 }
 
 /**
